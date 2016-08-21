a7aef2a8c39cd52a91dc36cc9ea8c26404db8da1
Craig44 <Craig.Marsh@niwa.co.nz>
2016-08-22 08:21:45 +1200

    ADD: Rstan model

--- Spatial Overlap statistic/SpatialOverlap.r
+++ Spatial Overlap statistic/SpatialOverlap.r
@@ -3,24 +3,24 @@
 ## @date 18/08/2016
 ## @description
 ##
-##	A script for running and estimating the spatial Tag Overlap Statistic (TSO) 
-##	reference (). Basically this method estimates the biomass of population over 
-##	a spatial grid at a single point in time. The biomass in each cell is unknown so we estimate all parameters 
-##	By fitting to data in a Bayesian framework. Observations used to inform our unknown 
-##	biomass is relative abundance (Commercial catch rate data) and mark recapture observations.
-## 	The overlap statistics are then summaries of how this biomass is distributed spatially at a 
-##	point in time, and which aspect is informed my mark recapture observations for diagnosing 
-## 	research plans.
+##  A script for running and estimating the spatial Tag Overlap Statistic (TSO) 
+##  reference (). Basically this method estimates the biomass of population over 
+##  a spatial grid at a single point in time. The biomass in each cell is unknown so we estimate all parameters 
+##  By fitting to data in a Bayesian framework. Observations used to inform our unknown 
+##  biomass is relative abundance (Commercial catch rate data) and mark recapture observations.
+##  The overlap statistics are then summaries of how this biomass is distributed spatially at a 
+##  point in time, and which aspect is informed my mark recapture observations for diagnosing 
+##  research plans.
 
 
 ## Things that need resolving/Achieved
-#	- Data weighting/conflicts
-#	- incorporate a Time Series
-# 	- Playing with RStan =)
+# - Data weighting/conflicts
+# - incorporate a Time Series
+# - Playing with RStan =) ~ q as a random effect (Make sure you have a good reason why we are trying this)
 
-
+####################
 ## Utility Functions
-
+####################
 ## @Rlnorm: Simulate data from an expected state, with a given biomass
 ## 
 ## @param mu: A vector of means (in log space) of each expected biomass
@@ -32,68 +32,79 @@ Rlnorm = function(n = 1, mu, sigma) {
   x
 }
 
-
+####################
 ## Global Variables
-Cells_ = 11; ## Number of spatial cells
+####################
 set.seed(23);
+Cells_ = 11; ## Number of spatial cells
 
-#|-------------------------
-#|B_1 |B_2 |B_3 |B_4 |B_5 |
-#|----|----|----|----|----|
-#|B_6 |B_7 |B_8 |B_9 |B_10|
-#|-------------------------
-
-
-## Partition
-labels = paste("B_" , 1:Cells_, sep = "");
-Partition_ = matrix(0,nrow = 1, ncol = Cells_);
-colnames(Partition_) = labels
-
-## Parameters
-
-
-## Partition
-
-
-#################
+####################
 ## Observations
-#################
+####################
 ## CPUE parameters
 sigma = 0.2; # std deviation of CPUE data
 # Our Observations from CPUE fishery
-obs = Rlnorm(mu = c(20.085537, 400,    64.56,   32.3526,  148.413159, 403.428793,   24.3452,   59.3425 ,  20.085537,   54.598150, 403.428793), sigma = sigma);
+mu = c(20.085537, 400,    64.56,   32.3526,  148.413159, 403.428793,   24.3452,   59.3425 ,  20.085537,   54.598150, 403.428793) * 100;
+obs = Rlnorm(mu = mu, sigma = sigma); ## 100 is a scaler for abundance
 ## Catchability
-q = 1.2
-## Mark recapture parameters
-## Lets generate data that wont conflict wiht the CPUE to begin
-
-
-## Commercial Catch Rates (CPUE) Well observed fishery;
-
-## Simulate data from an expected state, with a given error
-## @param mu: A vector of means (in log space) of each expected biomass
-## @param sigma: A vector of std deviations (in log space) of each observation
-## @param n: 
-
-Rlnorm = function(n = 1, mu, sigma) {
-  x = rlnorm(n = length(mu)*n, meanlog = log(mu), sdlog = sigma);
-  x;
-}
-scanned = c(1000, 2340, 633, 1200,1350, 324, 146,456,1364,1248, 213);
-recaptures = c(20, 15, 6, 12, 0, 2,8,2,0,3, 4);
-
-prob = recaptures/scanned
-rel = (obs * q) / prob; 
+q = 1.7;
 
 ## Mark-Recapture likelihood
 ## Each recapture and scanned individual in size bin follow a binomial random variable
-
-
-
+## Peterson estimate = (released * scanned) / recaptures
 ## A vector of scanned individuals 
-Scanned = c()
-recaptured = 
+scanned = c(587, 3000, 633, 120,60, 324, 146,456,364,548, 413);
+## Imaginary recaptures
+recaptures = c(4, 8, 6, 12, 0, 2,8,2,0,5, 6);
 
-## Age-Length Relationship
+prob = recaptures/scanned;
+rel = ((obs * q) * recaptures) / scanned;
 
-## Length-Weight Relationship
+
+## Generate a random sample from a binomial where n = scanned, p = recap / scanned
+(rel * scanned) / recaptures;
+Recaptures = rbinom(length(scanned), size = scanned, prob = prob);
+
+
+
+
+#|-------------------------
+#|B_1 |B_2 |B_3 |B_4 |B_5 |
+#|----|----|----|----|----|------|
+#|B_6 |B_7 |B_8 |B_9 |B_10| B_11 |
+#|-------------------------------|
+
+
+library(rstan);
+rstan_options(auto_write = TRUE);
+if (parallel::detectCores() > 2)
+  cores = 2
+options(mc.cores = cores);
+## Set wd
+if (Sys.info()[["nodename"]]=="NIWA-1007004") {
+  ## Work computer
+  setwd("C:/Craig/Personal/Projects/Spatial Overlap statistic\\");
+} else if (Sys.info()[["user"]]=="") {
+  ## Personal computer To be filled out
+  setwd("C:\\Craig\\projects\\2016\\ANT1601-01 (SpatialOverlap)\\");
+}
+
+
+####################
+## load model
+####################
+source("SpatialOverlap.rstan");
+#model = stanc(file = "SpatialOverlap.rstan", model_name = "Single_time_step");
+  
+####################
+## Load data 
+####################
+Data = list("S" = Cells_, "Scanned" = scanned, "Recaptured" = Recaptures, "Tagged" = rel, "Catch_rate" = obs, "Sigma" = sigma);
+
+
+fit <- stan(model_code = SpatialOverlapModel, model_name = "SpatialOverlap", data = Data,diagnostic_file = "diagnostic_file.out");
+
+
+
+
+
